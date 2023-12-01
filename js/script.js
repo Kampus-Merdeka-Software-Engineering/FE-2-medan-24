e69d5fad4919fbd041166c7b147cd91c9765f4d0
Muhammad Haris <2010631170098@student.unsika.ac.id>
2023-12-01 13:45:20 +0700

    Menambahkan isi content pada halaman home, booking, dan our team

--- js/script.js
+++ js/script.js
@@ -14,17 +14,6 @@ navToggle.addEventListener('click', () => {
     }
 });
 
-// SWIPER HOME
-var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
 // FILTER MENU  
 let list = document.querySelectorAll('.list');
 let itemBox = document.querySelectorAll('.itemBox');
@@ -52,17 +41,177 @@ for (let i = 0; i < list.length;
     })
   }
 
-// SWIPER TESTIMONIALS
-// const swiper = new Swiper('.js-testimonials-slider',  {
-//   grabCursor: true,
-//   spaceBetween: 30,
-//   pagination: {
-//     el: '.js-testimonials-pagination',
-//     clickable: true 
-//   },
-//   breakpoints:{
-//     767: {
-//       slidesPerView: 2
-//     }
-//   }
-// });
+
+
+// Booking FORM
+const bookedSlots = []; // Array untuk menyimpan slot yang sudah dipesan
+
+// Memeriksa apakah sudah ada data booking di localStorage
+const storedBookings = JSON.parse(localStorage.getItem('bookedSlots'));
+if (storedBookings) {
+  bookedSlots.push(...storedBookings);
+  displayBooking(); // Menampilkan data booking yang sudah tersimpan
+}
+
+function saveBooking() {
+  const table = document.getElementById('table').value;
+  const hours = document.getElementById('hours').value;
+
+  // Periksa apakah meja dan jam yang dipilih sudah dipesan sebelumnya
+  if (isSlotBooked(table, hours)) {
+    alert('Meja dan jam tersebut sudah dipesan. Silakan pilih meja atau jam yang lain.');
+    return;
+  }
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const date = document.getElementById('date').value;
+  const person = document.getElementById('person').value;
+  const specialRequest = document.getElementById('specialRequest').value;
+
+  // Simpan data booking ke localStorage
+  const newBooking = { table, hours, name, email, date, person, specialRequest };
+  bookedSlots.push(newBooking);
+  localStorage.setItem('bookedSlots', JSON.stringify(bookedSlots));
+
+  // Buat baris baru untuk ditambahkan ke tabel
+  const newRow = document.createElement('tr');
+  newRow.innerHTML = `
+    <td>${table}</td>
+    <td>${hours}</td>
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${date}</td>
+    <td>${person}</td>
+    <td>${specialRequest}</td>
+    <td class="action-buttons">
+      <button onclick="editBooking(this.parentNode.parentNode)">Edit</button>
+      <button onclick="deleteBooking(this.parentNode.parentNode)">Hapus</button>
+    </td>
+  `;
+
+  // Tambahkan baris ke tabel
+  const tableBody = document.getElementById('bookingTableBody');
+  tableBody.appendChild(newRow);
+
+  // Bersihkan nilai formulir setelah menyimpan
+  document.getElementById('bookingForm').reset();
+}
+
+function displayBooking() {
+  const tableBody = document.getElementById('bookingTableBody');
+  tableBody.innerHTML = '';
+
+  bookedSlots.forEach(booking => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${booking.table}</td>
+      <td>${booking.hours}</td>
+      <td>${booking.name}</td>
+      <td>${booking.email}</td>
+      <td>${booking.date}</td>
+      <td>${booking.person}</td>
+      <td>${booking.specialRequest}</td>
+      <td class="action-buttons">
+        <button onclick="editBooking(this.parentNode.parentNode)">Edit</button>
+        <button onclick="deleteBooking(this.parentNode.parentNode)">Hapus</button>
+      </td>
+    `;
+    tableBody.appendChild(newRow);
+  });
+}
+
+function isSlotBooked(table, hours) {
+  return bookedSlots.some(booking => booking.table === table && booking.hours === hours);
+}
+
+function filterTable() {
+  const searchValue = document.getElementById('search').value.toLowerCase();
+  const tableBody = document.getElementById('bookingTableBody');
+  const rows = tableBody.getElementsByTagName('tr');
+
+  for (let row of rows) {
+    const name = row.getElementsByTagName('td')[2].innerText.toLowerCase();
+
+    if (name.includes(searchValue)) {
+      row.style.display = '';
+    } else {
+      row.style.display = 'none';
+    }
+  }
+}
+
+function sortTable() {
+  const sortBy = document.getElementById('sort').value;
+  const tableBody = document.getElementById('bookingTableBody');
+  const rows = Array.from(tableBody.getElementsByTagName('tr'));
+
+  rows.sort((a, b) => {
+    const aValue = a.getElementsByTagName('td')[sortBy === 'hours' ? 1 : 0].innerText;
+    const bValue = b.getElementsByTagName('td')[sortBy === 'hours' ? 1 : 0].innerText;
+
+    if (sortBy === 'date') {
+      return new Date(aValue) - new Date(bValue);
+    } else {
+      return aValue.localeCompare(bValue);
+    }
+  });
+
+  // Hapus semua baris dari tabel
+  tableBody.innerHTML = '';
+
+  // Tambahkan baris yang telah diurutkan kembali ke tabel
+  rows.forEach(row => tableBody.appendChild(row));
+}
+
+function deleteBooking(row) {
+  const table = row.getElementsByTagName('td')[0].innerText;
+  const hours = row.getElementsByTagName('td')[1].innerText;
+
+  // Hapus baris dari tabel
+  row.remove();
+
+  // Hapus slot yang sudah dipesan
+  const index = bookedSlots.findIndex(slot => slot.table === table && slot.hours === hours);
+  if (index !== -1) {
+    bookedSlots.splice(index, 1);
+  }
+
+  // Perbarui data di localStorage
+  localStorage.setItem('bookedSlots', JSON.stringify(bookedSlots));
+}
+
+function editBooking(row) {
+  // ... (Kode fungsi editBooking sebelumnya)
+  const table = row.getElementsByTagName('td')[0].innerText;
+  const hours = row.getElementsByTagName('td')[1].innerText;
+  const name = row.getElementsByTagName('td')[2].innerText;
+  const email = row.getElementsByTagName('td')[3].innerText;
+  const date = row.getElementsByTagName('td')[4].innerText;
+  const person = row.getElementsByTagName('td')[5].innerText;
+  const specialRequest = row.getElementsByTagName('td')[6].innerText;
+
+  // Isi formulir edit dengan data yang ada
+  document.getElementById('table').value = table;
+  document.getElementById('hours').value = hours;
+  document.getElementById('name').value = name;
+  document.getElementById('email').value = email;
+  document.getElementById('date').value = date;
+  document.getElementById('person').value = person;
+  document.getElementById('specialRequest').value = specialRequest;
+  
+  // Hapus baris dari tabel
+  row.remove();
+
+  // Perbarui data di localStorage
+  localStorage.setItem('bookedSlots', JSON.stringify(bookedSlots));
+}
+
+// let form = document.getElementById("bookingForm");
+// let table = document.getElementById("table");
+// let hours = document.getElementById("hours");
+// let name = document.getElementById("name");
+// let email = document.getElementById("email");
+// let date = document.getElementById("date");
+// let person = document.getElementById("person");
+// let specialRequest = document.getElementById("specialRequest");
---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+//  SWIPER HOME
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// SWIPER TESTIMONIALS
+const swiper2 = new Swiper('.js-testimonials-slider',  {
+  grabCursor: true,
+  spaceBetween: 30,
+  pagination: {
+    el: '.js-testimonials-pagination',
+    clickable: true 
+  },
+  breakpoints:{
+    767: {
+      slidesPerView: 1
+    }
+  }
+});
