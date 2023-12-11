3f99ff010223b106757082dca55dc55beab5fde3
Muhammad Haris <2010631170098@student.unsika.ac.id>
2023-12-11 14:55:41 +0700

    Feat: Menambahkan function swiper

--- js/script.js
+++ js/script.js
@@ -41,29 +41,3 @@ for (let i = 0; i < list.length;
     })
   }
 
-
-  //  SWIPER HOME
-var swiper = new Swiper(".home", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// SWIPER TESTIMONIALS
-const swiper2 = new Swiper('.js-testimonials-slider',  {
-grabCursor: true,
-spaceBetween: 30,
-pagination: {
-  el: '.js-testimonials-pagination',
-  clickable: true 
-},
-breakpoints:{
-  767: {
-    slidesPerView: 1
-  }
-}
-});
